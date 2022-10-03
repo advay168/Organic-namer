066fce6c28cf2bc0458a0a5e252dde7965097de3
Advay <23453652+advay168@users.noreply.github.com>
2022-10-03 19:34:53 +0530

    Find longest chain

--- src/Application/Namer.cpp
+++ src/Application/Namer.cpp
@@ -152,7 +152,7 @@ Namer::SingleAtom* Namer::findTerminalCarbonAtom(SingleAtom* carbonAtom)
     {
         atom->visited = false;
     }
-    std::function<SingleAtom*(SingleAtom*)> helper = [&helper](auto carbonAtom)
+    std::function<SingleAtom*(SingleAtom*)> helper = [&helper](SingleAtom* carbonAtom)
     {
         carbonAtom->visited = true;
         for (auto ba : carbonAtom->bondedAtoms)
@@ -165,13 +165,179 @@ Namer::SingleAtom* Namer::findTerminalCarbonAtom(SingleAtom* carbonAtom)
     return helper(carbonAtom);
 }
 
+std::vector<Namer::SingleAtom*> Namer::findMaxCarbonChain(SingleAtom* carbonAtom)
+{
+    for (auto& atom : atomsList)
+    {
+        atom->visited = false;
+    }
+    using ret_t = std::vector<Namer::SingleAtom*>;
+    std::function<ret_t(SingleAtom*)> helper = [&helper](SingleAtom* carbonAtom) -> ret_t
+    {
+        carbonAtom->visited = true;
+        auto carbs = carbonAtom->findAtoms<ElementType::Carbon>();
+        uint32_t maxLength = 0;
+        ret_t ret;
+        for (auto atom : carbs)
+        {
+            if (!atom->visited)
+            {
+                auto path = helper(atom);
+                if (path.size() > maxLength)
+                {
+                    maxLength = path.size();
+                    ret = path;
+                }
+            }
+        }
+        ret.push_back(carbonAtom);
+        return ret;
+    };
+    auto path = helper(carbonAtom);
+    std::reverse(path.begin(), path.end());
+    return path;
+}
+
+Namer::FunctionalGroup Namer::findHighestPriorityGroup(const std::vector<SingleAtom*>& chain)
+{
+    for (SingleAtom* atom : chain)
+    {
+    }
+    return FunctionalGroup::ALKANE;
+}
+
 std::string Namer::nameOrganic(SingleAtom* carbonAtom)
 {
-    ((Atom*)carbonAtom->correspondent)->color = glm::vec3(1.0f);
-    return "CH4";
+    auto path = findMaxCarbonChain(carbonAtom);
+    for (SingleAtom* atom : path)
+        ((Atom*)atom->correspondent)->color = glm::vec3(1.0f);
+    int size = path.size();
+    std::string prefix = namePrefix(size);
+    FunctionalGroup group = findHighestPriorityGroup(path);
+
+    return join(prefix, nameSuffix(group));
 }
 
 std::string Namer::nameInorganic()
 {
     return "CO2";
+}
+
+std::string Namer::namePrefix(int n)
+{
+    switch (n)
+    {
+    case 1:
+        return "metha";
+    case 2:
+        return "etha";
+    case 3:
+        return "propa";
+    case 4:
+        return "buta";
+    case 5:
+        return "penta";
+    case 6:
+        return "hexa";
+    case 7:
+        return "hepta";
+    case 8:
+        return "octa";
+    case 9:
+        return "nona";
+    case 10:
+        return "deca";
+    default:
+        throw std::runtime_error("Don't know prefix of large chain");
+    }
+}
+
+std::string Namer::nameSuffix(FunctionalGroup group)
+{
+    switch (group)
+    {
+    case FunctionalGroup::CARBOXYLIC_ACID:
+        return "";
+    case FunctionalGroup::ESTER:
+        return "";
+    case FunctionalGroup::AMIDE:
+        return "";
+    case FunctionalGroup::NITRILE:
+        return "";
+    case FunctionalGroup::ALDEHYDE:
+        return "";
+    case FunctionalGroup::KETONE:
+        return "";
+    case FunctionalGroup::ALCOHOL:
+        return "";
+    case FunctionalGroup::AMINE:
+        return "";
+    case FunctionalGroup::ALKYNE:
+        return "";
+    case FunctionalGroup::ALKENE:
+        return "";
+    case FunctionalGroup::ALKANE:
+        return "ane";
+    }
+}
+
+std::string Namer::namePrefix(FunctionalGroup group)
+{
+    switch (group)
+    {
+    case FunctionalGroup::CARBOXYLIC_ACID:
+        return "";
+    case FunctionalGroup::ESTER:
+        return "";
+    case FunctionalGroup::AMIDE:
+        return "";
+    case FunctionalGroup::NITRILE:
+        return "";
+    case FunctionalGroup::ALDEHYDE:
+        return "";
+    case FunctionalGroup::KETONE:
+        return "";
+    case FunctionalGroup::ALCOHOL:
+        return "";
+    case FunctionalGroup::AMINE:
+        return "";
+    case FunctionalGroup::ALKYNE:
+        return "";
+    case FunctionalGroup::ALKENE:
+        return "";
+    case FunctionalGroup::ALKANE:
+        return "";
+    }
+}
+
+std::string Namer::join(const std::string& a, const std::string& b)
+{
+    auto isVowel = [](char c)
+    {
+        switch (c)
+        {
+        case 'a':
+        case 'A':
+        case 'e':
+        case 'E':
+        case 'i':
+        case 'I':
+        case 'o':
+        case 'O':
+        case 'u':
+        case 'U':
+        case 'y':
+        case 'Y':
+            return true;
+        default:
+            return false;
+        }
+    };
+    bool aVowel = isVowel(a.back());
+    bool bVowel = isVowel(b.front());
+    if (aVowel && bVowel)
+    {
+        return a.substr(0, a.size() - 1) + b;
+    }
+    return a + b;
 }
--- src/Application/Namer.h
+++ src/Application/Namer.h
@@ -38,15 +38,50 @@ private:
         std::vector<std::tuple<BondedAtom, BondedAtom>> getDoubleBonds();
         std::vector<std::tuple<BondedAtom, BondedAtom, BondedAtom>> getTripleBonds();
 
+        template <ElementType::ElementTypeEnum el>
+        std::vector<SingleAtom*> findAtoms()
+        {
+            std::vector<SingleAtom*> ret;
+            for (auto& ba : bondedAtoms)
+            {
+                if (ba.bondedAtom->element == el && ba.isSameBond == false)
+                    ret.push_back(ba.bondedAtom);
+            }
+            return ret;
+        }
+
         std::vector<BondedAtom> getUniqueBonds();
+    };
+
+    enum class FunctionalGroup : uint8_t
+    {
+        CARBOXYLIC_ACID = 56,
+        ESTER,
+        AMIDE,
+        NITRILE,
+        ALDEHYDE,
+        KETONE,
+        ALCOHOL,
+        AMINE,
+        ALKYNE,
+        ALKENE,
+        ALKANE
     };
 
     void sortBonds();
     SingleAtom* findCarbonAtom();
     SingleAtom* findTerminalCarbonAtom(SingleAtom* carbonAtom);
+    std::vector<SingleAtom*> findMaxCarbonChain(SingleAtom* carbonAtom);
+    FunctionalGroup findHighestPriorityGroup(const std::vector<SingleAtom*>& chain);
 
     std::string nameOrganic(SingleAtom* carbonAtom);
     std::string nameInorganic();
+
+    std::string namePrefix(int n);
+    std::string namePrefix(FunctionalGroup group);
+    std::string nameSuffix(FunctionalGroup group);
+    std::string join(const std::string& a, const std::string& b);
+
 
 private:
     std::unordered_map<const Atom*, SingleAtom*> atomMapping;
